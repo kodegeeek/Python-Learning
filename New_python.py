42b9d9756eb203fc2665d4d4fe0f665c9b2428a6
kodegeeek <74041377+kodegeeek@users.noreply.github.com>
2022-06-27 05:55:13 +0530

    Add files via upload

---
+++ New_python.py
@@ -0,0 +1,1 @@
+print('Hello vikas , My parent is app.py')
---
+++ app.py
@@ -0,0 +1,18 @@
+# Classes and Objects in python
+# anything that deals with classes and objects is OOP- object oreanted programming
+
+
+# Creating a class
+
+class Myclass:
+    x = 5
+
+p1= Myclass()
+print(p1)# prints the address the object 'myclass'
+print(p1.x)#5
+
+
+
+class person:
+    def __init__(self,name,age):
+        self
